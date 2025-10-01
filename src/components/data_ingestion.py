3087cbcbde5d9865e6bbb69a2127c3c7fff09783
Prem <katyain1prem@gmail.com>
2025-10-02 02:06:25 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact','train.csv')
@@ -23,7 +27,7 @@ class DataIngestion:
             df=pd.read_csv(r'notebook\Data\stud.csv')
             logging.info('Read the dataset as dataframe')
             
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #creates the directory artifact
             
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             
@@ -46,5 +50,8 @@ class DataIngestion:
             raise CustomException(e,sys)
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
